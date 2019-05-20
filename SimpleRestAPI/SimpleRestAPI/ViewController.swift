3b7e5d8109715a370f2467961647ada476e005d5
veerababum <veerababum@users.noreply.github.com>
2019-05-20 16:46:43 -0600

    Add files via upload

---
+++ SimpleRestAPI/SimpleRestAPI/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  SimpleRestAPI
+//
+//  Created by Veerababu Mulugu on 5/20/19.
+//  Copyright © 2019 Veerababu Mulugu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    func makeGetCall() {
+        // Set up URL request
+        
+        let todoEndPoint: String = "https://jsonplaceholder.typicode.com/todos/1"
+        
+        
+        
+        
+        
+    }
+    
+    
+}
+
