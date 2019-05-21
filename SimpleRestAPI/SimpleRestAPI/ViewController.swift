7dc5ded0a2d8f638929a7b9d1bb6b60aae1b473f
Veerababu Mulugu <veeru060786@gmail.com>
2019-05-21 11:55:28 -0600

    Callled the APi and printed the response.

--- SimpleRestAPI/SimpleRestAPI/ViewController.swift
+++ SimpleRestAPI/SimpleRestAPI/ViewController.swift
@@ -13,9 +13,51 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        makeGetCall()
     }
 
-   
+    func makeGetCall() {
+        //Set up the URL request
     
+        let task = session.dataTask(with: URLRequest) {
+            (data, response, error) in
+            // check for any errors
+            guard error == nil else {
+                print("error calling GET on /todos/1")
+                print(error!)
+                return
+            }
+            // make sure we got data
+            guard let responseData = data else {
+                print("Error: did not receive data")
+                return
+            }
+            // parse the result as JSON, since that's what the API provides
+            do {
+                guard let todo = try JSONSerialization.jsonObject(with: responseData, options: [])
+                    as? [String: Any] else {
+                        print("error trying to convert data to JSON")
+                        return
+                }
+                // now we have the todo
+                // let's just print it to prove we can access it
+                print("The todo is: " + todo.description)
+                
+                // the todo object is a dictionary
+                // so we just access the title using the "title" key
+                // so check for a title and print it if we have one
+                guard let todoTitle = todo["title"] as? String else {
+                    print("Could not get todo title from JSON")
+                    return
+                }
+                print("The title is: " + todoTitle)
+            } catch  {
+                print("error trying to convert data to JSON")
+                return
+            }
+        }
+        task.resume()
+    }
 }
 
