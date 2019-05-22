3be17676ce9d3915f71df34a7733faf00682ae3d
Veerababu Mulugu <veeru060786@gmail.com>
2019-05-22 16:22:04 -0600

    UItableview integrated.
    added th euitableview and added the array values and displayed on the uitable.

--- SimpleRestAPI/SimpleRestAPI/ViewController.swift
+++ SimpleRestAPI/SimpleRestAPI/ViewController.swift
@@ -8,17 +8,63 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    struct Headline {
+        
+        var id : Int
+        var title : String
+        var text : String
+        var image : String
+        
+    }
+    
+    var headlines = [
+        Headline(id: 1, title: "Lorem Ipsum", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", image: "Apple"),
+        Headline(id: 2, title: "Aenean condimentum", text: "Ut eget massa erat. Morbi mauris diam, vulputate at luctus non.", image: "Banana"),
+        Headline(id: 3, title: "In ac ante sapien", text: "Aliquam egestas ultricies dapibus. Nam molestie nunc.", image: "Cantaloupe"),
+    ]
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
         
 //        makeGetCall()
 //        makePostCall()
-        makeDeleteCall()
-
+       // makeDeleteCall()
+        
     }
+ //***************************************
+    //Ui tableview controller delegate methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return headlines.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = headlines[indexPath.row].title
+        cell.detailTextLabel?.text = headlines[indexPath.row].text
+        
+        
+        return cell
+        
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //**************************
     func makePostCall(){
   
         let todosEndpoint: String = "https://jsonplaceholder.typicode.com/todos"
